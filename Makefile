8be30e93726c756cfd87088454177c86b0d46882
palich2000 <y.palich.t@gmail.com>
2018-01-16 13:47:30 +0200

    add daemon mode

--- Makefile
+++ Makefile
@@ -2,9 +2,9 @@ CC = gcc
 
 CFLAGS = -g -std=c11 -MD -MP  -Wall -Wfatal-errors
 
-OBJGROUP = si1132.o bme280-i2c.o bme280.o weather_board.o
+OBJGROUP = si1132.o bme280-i2c.o bme280.o weather_board.o dlog.o dpid.o dfork.o dexec.o dsignal.o dzip.o dmem.o dnonblock.o version.o
 
-EXTRA_LIBS = -lwiringPi -lpthread -lcrypt -lrt
+EXTRA_LIBS = -lwiringPi -lpthread -lcrypt -lrt -lzip
 
 all: weather_board
 
@@ -24,3 +24,20 @@ clean:
 
 install: weather_board
 	install -D -o root -g root ./weather_board /usr/local/bin
+
+
+#######################
+G_EX = $(shell git describe --tag > /dev/null ; if [ $$? -eq 0 ]; then echo "OK"; else echo "FAIL" ; fi)
+GVER = $(shell git describe --abbrev=7 --long)
+#######################
+
+version.c: FORCE
+	@echo "==============================================="
+	@echo "git present:" $(G_EX) " ver:" $(GVER)
+	@echo "==============================================="
+ifeq "$(G_EX)" "OK"
+	git describe --tag | awk 'BEGIN { FS="-" } {print "#include \"version.h\""} {print "const char * git_version = \"" $$1"."$$2"\";"} END {}' > version.c
+	git rev-parse --abbrev-ref HEAD | awk '{print "const char * git_branch = \""$$0"\";"} {}' >> version.c
+endif
+
+FORCE:
--- weather_board.c
+++ weather_board.c
@@ -6,10 +6,23 @@
 #include <errno.h>
 #include <time.h>
 #include <signal.h>
+#include <stdbool.h>
+#include <pthread.h>
+#include <sys/sysinfo.h>
+#include <sys/time.h>
+#include <sys/resource.h>
+#include <sys/syscall.h>
+#include <sys/wait.h>
+
 #include "bme280-i2c.h"
 #include "si1132.h"
 
-const char version[] = "v1.6";
+#include "dpid.h"
+#include "dmem.h"
+#include "dlog.h"
+#include "dfork.h"
+#include "dsignal.h"
+#include "version.h"
 
 static int pressure;
 static int temperature;
@@ -19,15 +32,90 @@ static int humidity;
 #define  O_JSON 1
 
 float SEALEVELPRESSURE_HPA = 1024.25;
-static char * progname = NULL;
 static int out_format = O_TEXT;
 static char * out_filename = NULL;
 static FILE * out_file = NULL;
-static void usage() {
 
+#define HOSTNAME_SIZE 256
+#define CDIR "./"
+
+static char * progname = NULL;
+static char * hostname = NULL;
+static char * pathname = NULL;
+static const char * const application = "weather_board";
+static int do_exit = 0;
+
+static void usage() {
     fprintf(stderr, "Usage: %s [-d ] [-f] [-p integer] [-k command] [-w integer] \n", progname);
     exit(1);
 }
+
+//------------------------------------------------------------------------------------------------------------
+//
+// Daemon commands callbacks:
+//
+//------------------------------------------------------------------------------------------------------------
+pid_t main_pid;
+
+enum command_int_t {
+    CMD_NONE = 0,
+    CMD_RECONFIGURE,
+    CMD_SHUTDOWN,
+    CMD_RESTART,
+    CMD_CHECK,
+    CMD_NOT_FOUND = -1,
+};
+
+typedef int (* daemon_command_callback_t)(void*);
+
+typedef struct daemon_command_t {
+    char * command_name;
+    daemon_command_callback_t  command_callback;
+    int command_int;
+} DAEMON_COMMAND_T;
+
+int check_callback(void * UNUSED(param)) {
+    return(10);
+}
+
+
+int reconfigure_callback(void * UNUSED(param)) {
+
+    if (daemon_pid_file_kill(SIGUSR1) < 0) {
+        daemon_log(LOG_WARNING, "Failed to reconfiguring");
+    } else {
+        daemon_log(LOG_INFO, "OK");
+    }
+    return(10);
+}
+
+int shutdown_callback(void * UNUSED(param)) {
+    int ret;
+    daemon_log(LOG_INFO, "Try to shutdown self....");
+    if ((ret = daemon_pid_file_kill_wait(SIGINT, 10)) < 0) {
+        daemon_log(LOG_WARNING, "Failed to shutdown daemon %d %s", errno, strerror(errno));
+        daemon_log(LOG_WARNING, "Try to terminating self....");
+        if (daemon_pid_file_kill_wait(SIGKILL, 0) < 0) {
+            daemon_log(LOG_WARNING, "Failed to killing daemon %d %s", errno, strerror(errno));
+        } else {
+            daemon_log(LOG_WARNING, "Daemon terminated");
+        }
+    } else
+        daemon_log(LOG_INFO, "OK");
+    return(10);
+}
+
+int restart_callback(void * UNUSED(param)) {
+    shutdown_callback(NULL);
+    return(0);
+}
+
+DAEMON_COMMAND_T daemon_commands[] = {
+    {command_name: "reconfigure", command_callback: reconfigure_callback, command_int: CMD_RECONFIGURE},
+    {command_name: "shutdown", command_callback: shutdown_callback, command_int: CMD_SHUTDOWN},
+    {command_name: "restart", command_callback: restart_callback, command_int: CMD_RESTART},
+    {command_name: "check", command_callback: check_callback, command_int: CMD_CHECK},
+};
 
 char *arg_param(char *arg) {
     char *p = strchr(arg, ':');
@@ -45,7 +133,7 @@ void open_outfile() {
         out_file = fopen(out_filename, "a");
     }
     if (!out_file) {
-        fprintf(stderr, "Unable to open out file %d %s", errno, strerror(errno));
+        daemon_log(LOG_ERR, "Unable to open out file %d %s", errno, strerror(errno));
     }
 }
 
@@ -61,8 +149,8 @@ void out_text() {
     fprintf(out_file, "IR : %.0f Lux\e[K\n", Si1132_readIR());
 
     if (bme280_read_pressure_temperature_humidity(
-        (u32*)&pressure, &temperature, (u32*)&humidity) == -1) {
-	fprintf(stderr, "%s Error communication with bme280\n", __FUNCTION__);
+                (u32*)&pressure, &temperature, (u32*)&humidity) == -1) {
+        daemon_log(LOG_ERR, "%s Error communication with bme280", __FUNCTION__);
     }
     fprintf(out_file, "======== bme280 ========\n");
     fprintf(out_file, "temperature : %.2lf 'C\e[K\n", (double)temperature / 100.0);
@@ -82,9 +170,11 @@ void out_json() {
     tm_info = localtime(&timer);
     strftime(buffer, sizeof(buffer) - 1, "%Y-%m-%d %H:%M:%S", tm_info);
     if (bme280_read_pressure_temperature_humidity(
-        (u32*)&pressure, &temperature, (u32*)&humidity) == -1) {
-	fprintf(stderr, "%s Error communication with bme280\n", __FUNCTION__);
+                (u32*)&pressure, &temperature, (u32*)&humidity) == -1) {
+        daemon_log(LOG_ERR, "%s Error communication with bme280", __FUNCTION__);
+        return;
     }
+
     fprintf(out_file,
             "{\"time\": \"%s\", \"brand\": \"ODROID\", \"model\": \"WB2\", \"id\": 0, \"channel\": 1, \"battery\": \"OK\", \
 \"temperature_C\": %.2lf, \"humidity\": %.2lf, \"pressure\": %.2lf, \"altitude\": %f, \
@@ -92,77 +182,12 @@ void out_json() {
             (double)temperature / 100.0, (double)humidity / 1024.0, (double)pressure / 100.0, bme280_readAltitude(pressure, SEALEVELPRESSURE_HPA),
             Si1132_readUV() / 100.0, Si1132_readVisible(), Si1132_readIR() );
     fflush(out_file);
-//{"time" : "2017-10-13 13:28:37", "brand" : "OS", "model" : "THGR122N", "id" : 88, "channel" : 1, "battery" : "OK", "temperature_C" : 14.000, "humidity" : 65}
 }
 
-static int do_exit = 0;
 
-static void sighandler(int signum) {
-    if (signum == SIGPIPE) {
-        signal(SIGPIPE, SIG_IGN);
-    } else {
-        fprintf(stderr, "Signal caught, exiting!\n");
-    }
-    do_exit = 1;
-}
-
-int main(int argc, char **argv) {
-    int flags = 0;
-    char *device = "/dev/i2c-1";
-    struct sigaction sigact;
-
-    sigact.sa_handler = sighandler;
-    sigemptyset(&sigact.sa_mask);
-    sigact.sa_flags = 0;
-    sigaction(SIGINT, &sigact, NULL);
-    sigaction(SIGTERM, &sigact, NULL);
-    sigaction(SIGQUIT, &sigact, NULL);
-    sigaction(SIGPIPE, &sigact, NULL);
-
-
-    if ((progname = strrchr(argv[0], '/')) == NULL)
-        progname = argv[0];
-    else
-        ++progname;
-
-    while ((flags = getopt(argc, argv, "F:")) != -1) {
-        switch (flags) {
-
-        case 'o': {
-            break;
-        }
-
-        case 'F': {
-            if (strncmp(optarg, "json", 4) == 0) {
-                out_format = O_JSON;
-                out_filename = arg_param(optarg);
-            } else if (strncmp(optarg, "text", 4) == 0) {
-                out_format = O_TEXT;
-                out_filename = arg_param(optarg);
-            } else {
-                fprintf(stderr, "Invalid output format %s\n", optarg);
-                usage();
-            }
-            break;
-        }
-        case 'd': {
-            device = strdup(optarg);
-            break;
-        }
-        default: {
-            usage();
-            break;
-        }
-        }
-    }
-
-    si1132_begin(device);
-    bme280_begin(device);
-
-    fprintf(stderr, "WEATHER-BOARD %s\n", version);
-
-    open_outfile();
-
+static
+void * main_loop (void * p) {
+    daemon_log(LOG_INFO, "%s started", __FUNCTION__);
     while (!do_exit) {
 
         if ((out_file != stdout) && (access(out_filename, F_OK) == -1)) {
@@ -175,9 +200,288 @@ int main(int argc, char **argv) {
         } else {
             out_json();
         }
-        usleep(20000000);
+
+        int c_delay = 0;
+        while ((!do_exit) && (c_delay < 20)) {
+            sleep(1);
+            c_delay++;
+        }
+
     }
 
-    close_outfile();
-    return 0;
+    daemon_log(LOG_INFO, "%s finished", __FUNCTION__);
+    return NULL;
 }
+
+//------------------------------------------------------------------------------------------------------------
+//
+// Start Program:
+//
+//------------------------------------------------------------------------------------------------------------
+
+int
+main (int argc, char *const *argv) {
+    int flags;
+    int daemonize = true;
+    int debug = 0;
+    char * command = NULL;
+    pid_t pid;
+    pthread_t main_th = 0;
+    char *device = "/dev/i2c-1";
+
+    int    fd, sel_res;
+
+    daemon_pid_file_ident = daemon_log_ident = application;
+
+    tzset();
+
+    if ((progname = strrchr(argv[0], '/')) == NULL)
+        progname = argv[0];
+    else
+        ++progname;
+
+    if (strrchr(argv[0], '/') == NULL)
+        pathname = xstrdup(CDIR);
+    else {
+        pathname = xmalloc(strlen(argv[0]) + 1);
+        strncpy(pathname, argv[0], (strrchr(argv[0], '/') - argv[0]) + 1);
+    }
+
+    if (chdir(pathname) < 0) {
+        daemon_log(LOG_ERR, "chdir error: %s", strerror(errno));
+    }
+
+    FREE(pathname);
+
+    pathname = get_current_dir_name();
+
+    hostname = calloc(1, HOSTNAME_SIZE);
+    gethostname(hostname, HOSTNAME_SIZE - 1);
+
+    daemon_log_upto(LOG_INFO);
+    daemon_log(LOG_INFO, "%s %s", pathname, progname);
+
+    while ((flags = getopt(argc, argv, "i:fF:D:dk:")) != -1) {
+        switch (flags) {
+
+        case 'k': {
+            command = xstrdup(optarg);
+            break;
+        }
+        case 'i': {
+            daemon_pid_file_ident = daemon_log_ident = xstrdup(optarg);
+            break;
+        }
+        case 'f' : {
+            daemonize = false;
+            break;
+        }
+        case 'F': {
+            if (strncmp(optarg, "json", 4) == 0) {
+                out_format = O_JSON;
+                out_filename = arg_param(optarg);
+            } else if (strncmp(optarg, "text", 4) == 0) {
+                out_format = O_TEXT;
+                out_filename = arg_param(optarg);
+            } else {
+                daemon_log(LOG_ERR, "Invalid output format %s", optarg);
+                usage();
+            }
+            break;
+        }
+        case 'd': {
+            debug++;
+            daemon_log_upto(LOG_DEBUG);
+            break;
+        }
+        case 'D': {
+            device = strdup(optarg);
+            break;
+        }
+        default: {
+            usage();
+            break;
+        }
+        }
+    }
+
+    if (debug) {
+        daemon_log(LOG_DEBUG,    "**************************");
+        daemon_log(LOG_DEBUG,    "* WARNING !!! Debug mode *");
+        daemon_log(LOG_DEBUG,    "**************************");
+    }
+
+    daemon_log(LOG_INFO, "%s ver %s [%s %s %s] started", application,  git_version, git_branch, __DATE__, __TIME__);
+    daemon_log(LOG_INFO, "***************************************************************************");
+    daemon_log(LOG_INFO, "pid file: %s", daemon_pid_file_proc());
+    if (command) {
+        int r = CMD_NOT_FOUND;
+        for (unsigned int i = 0; i < (sizeof(daemon_commands) / sizeof(daemon_commands[0])); i++) {
+            if ((strcasecmp(command, daemon_commands[i].command_name) == 0) && (daemon_commands[i].command_callback)) {
+                if ((r = daemon_commands[i].command_callback(pathname)) != 0) exit(abs(r - 10));
+            }
+        }
+        if (r == CMD_NOT_FOUND) {
+            daemon_log(LOG_ERR, "command \"%s\" not found.", command);
+            usage();
+        }
+    }
+    FREE(command);
+
+    /* initialize PRNG */
+    srand ((unsigned int) time (NULL));
+
+    if ((pid = daemon_pid_file_is_running()) >= 0) {
+        daemon_log(LOG_ERR, "Daemon already running on PID file %u", pid);
+        return 1;
+    }
+
+    daemon_log(LOG_INFO, "Make a daemon");
+
+    daemon_retval_init();
+    if ((daemonize) && ((pid = daemon_fork()) < 0)) {
+        return 1;
+    } else if ((pid) && (daemonize)) {
+        int ret;
+        if ((ret = daemon_retval_wait(20)) < 0) {
+            daemon_log(LOG_ERR, "Could not recieve return value from daemon process.");
+            return 255;
+        }
+        if (ret == 0) {
+            daemon_log(LOG_INFO, "Daemon started.");
+        } else {
+            daemon_log(LOG_ERR, "Daemon dont started, returned %i as return value.", ret);
+        }
+        return ret;
+    } else {
+
+        if (daemon_pid_file_create() < 0) {
+            daemon_log(LOG_ERR, "Could not create PID file (%s).", strerror(errno));
+            daemon_retval_send(1);
+            goto finish;
+        }
+
+        if (daemon_signal_init(/*SIGCHLD,*/SIGINT, SIGTERM, SIGQUIT, SIGHUP, SIGUSR1, SIGUSR2, SIGHUP, /*SIGSEGV,*/ 0) < 0) {
+            daemon_log(LOG_ERR, "Could not register signal handlers (%s).", strerror(errno));
+            daemon_retval_send(1);
+            goto finish;
+        }
+
+        daemon_retval_send(0);
+        daemon_log(LOG_INFO, "%s ver %s [%s %s %s] started", application,  git_version, git_branch, __DATE__, __TIME__);
+
+        struct rlimit core_lim;
+
+        if (getrlimit(RLIMIT_CORE, &core_lim) < 0) {
+            daemon_log(LOG_ERR, "getrlimit RLIMIT_CORE error:%s", strerror(errno));
+        } else {
+            daemon_log(LOG_INFO, "core limit is cur:%2ld max:%2ld", core_lim.rlim_cur, core_lim.rlim_max );
+            core_lim.rlim_cur = -1;
+            core_lim.rlim_max = -1;
+            if (setrlimit(RLIMIT_CORE, &core_lim) < 0) {
+                daemon_log(LOG_ERR, "setrlimit RLIMIT_CORE error:%s", strerror(errno));
+            } else {
+                daemon_log(LOG_INFO, "core limit set cur:%2ld max:%2ld", core_lim.rlim_cur, core_lim.rlim_max );
+            }
+        }
+
+        main_pid = syscall(SYS_gettid);
+
+        si1132_begin(device);
+        bme280_begin(device);
+        open_outfile();
+
+        pthread_create( &main_th, NULL, main_loop, NULL);
+// main
+
+        fd_set fds;
+        FD_ZERO(&fds);
+        fd = daemon_signal_fd();
+        FD_SET(fd,  &fds);
+
+        while (!do_exit) {
+            struct timeval tv;
+            tv.tv_sec  = 0;
+            tv.tv_usec = 100000;
+            fd_set fds2 = fds;
+            if ((sel_res = select(FD_SETSIZE, &fds2, 0, 0, &tv)) < 0) {
+
+                if (errno == EINTR)
+                    continue;
+
+                daemon_log(LOG_ERR, "select() error:%d %s", errno,  strerror(errno));
+                break;
+            }
+            if (FD_ISSET(fd, &fds2)) {
+                int sig;
+
+                if ((sig = daemon_signal_next()) <= 0) {
+                    daemon_log(LOG_ERR, "daemon_signal_next() failed.");
+                    break;
+                }
+
+                switch (sig) {
+                case SIGCHLD: {
+                    int ret = 0;
+                    daemon_log(LOG_INFO, "SIG_CHLD");
+                    wait(&ret);
+                    daemon_log(LOG_INFO, "RET=%d", ret);
+                }
+                break;
+
+                case SIGINT:
+                case SIGQUIT:
+                case SIGTERM:
+                    daemon_log(LOG_WARNING, "Got SIGINT, SIGQUIT or SIGTERM");
+                    do_exit = true;
+                    break;
+
+                case SIGUSR1: {
+                    daemon_log(LOG_WARNING, "Got SIGUSR1");
+                    daemon_log(LOG_WARNING, "Enter in debug mode, to stop send me USR2 signal");
+                    daemon_log_upto(LOG_DEBUG);
+                    break;
+                }
+                case SIGUSR2: {
+                    daemon_log(LOG_WARNING, "Got SIGUSR2");
+                    daemon_log(LOG_WARNING, "Leave debug mode");
+                    daemon_log_upto(LOG_INFO);
+                    break;
+                }
+                case SIGHUP:
+                    daemon_log(LOG_WARNING, "Got SIGHUP");
+                    break;
+
+                case SIGSEGV:
+                    daemon_log(LOG_ERR, "Seg fault. Core dumped to /tmp/core.");
+                    if (chdir("/tmp") < 0) {
+                        daemon_log(LOG_ERR, "Chdir to /tmp error: %s", strerror(errno));
+                    }
+                    signal(sig, SIG_DFL);
+                    kill(getpid(), sig);
+                    break;
+
+                default:
+                    daemon_log(LOG_ERR, "UNKNOWN SIGNAL:%s", strsignal(sig));
+                    break;
+
+                }
+            }
+        }
+
+    }
+
+finish:
+    daemon_log(LOG_INFO, "Exiting...");
+    pthread_join(main_th, NULL);
+    close_outfile();
+    FREE(hostname);
+    FREE(pathname);
+    daemon_retval_send(-1);
+    daemon_signal_done();
+    daemon_pid_file_remove();
+    daemon_log(LOG_INFO, "Exit");
+    exit(0);
+}
+//------------------------------------------------------------------------------------------------------------
+//------------------------------------------------------------------------------------------------------------
